93b2746eb81fa9394bee914312430c45fe99906b
Sam C <chuquest.work@gmail.com>
2025-08-22 18:46:09 +0100

    Timer extended

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -135,7 +135,7 @@ const Home = () => {
             navigate("/Expire");
         }
         else {
-            let targetDate = new Date("Aug 22, 2025 18:00:00").getTime();
+            let targetDate = new Date("Aug 31, 2025 23:00:00").getTime();
 
             //update the countdown every second 
             let update = setInterval(() => {
